9276c68de7ec2c60d687eb6cf48ed073be266f05
AZ <abdullahzahid6785@gmail.com>
2023-04-07 21:18:27 +0500

    welcome page aligned center

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -10,7 +10,7 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      //mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       //crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
