02326d9a5cbd321e4d42e5a5d3ee483d05ff814a
MaaziBhai-78 <hackergee3@gmail.com>
2023-05-03 13:09:25 +0500

    Dine IN added

---
+++ lib/pages/Customer/res_dine.dart
@@ -0,0 +1,109 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eatopia/pages/Customer/dine_in.dart';
+import 'package:eatopia/services/auth_services.dart';
+import 'package:eatopia/services/db.dart';
+import 'package:eatopia/services/maps.dart';
+import 'package:eatopia/utilities/colours.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:eatopia/pages/Customer/user_res_page.dart';
+
+class Res_Dine extends StatefulWidget {
+  const Res_Dine({Key? key}) : super(key: key);
+  @override
+  State<Res_Dine> createState() => _Res_DineState();
+}
+
+class _Res_DineState extends State<Res_Dine> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Restaurants'),
+        backgroundColor: appGreen,
+      ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('Restaurants').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.separated(
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var res = snapshot.data!.docs[index];
+              final doc = snapshot.data!.docs[index];
+              String resDesc = (doc.data() as Map<String, dynamic>)
+                      .containsKey('description')
+                  ? doc['description']
+                  : '';
+              String imageURL =
+                  (doc.data() as Map<String, dynamic>).containsKey('ImageURL')
+                      ? doc['ImageURL']
+                      : '';
+              bool isOpen =
+                  (doc.data() as Map<String, dynamic>).containsKey('isOpen')
+                      ? doc['isOpen']
+                      : false;
+              String address = doc['address'];
+              return Card(
+                elevation: 5,
+                shadowColor: Colors.grey.withOpacity(0.5),
+                child: InkWell(
+                  onTap: () {
+                    // navigate user res page
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            // builder: (context) => const Res_Dine()));
+                            builder: (context) => DineInPage()));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 100.0,
+                          width: double.infinity,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(7.0),
+                            child: Image.network(
+                              res['ImageURL'],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Text(
+                          res['restaurant'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 6.0),
+                        Text(
+                          res['address'],
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
